e9a62517c0c3f7ae1df8d63456be7d1112f60749
atitsbest <atitsbest@gmail.com>
2015-05-09 22:02:18 +0200

    + REDESIGN: Jetzt sollte es passen ;)

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -36,21 +36,21 @@ $(function() {
         }); // end window scroll
     });  // end section function
 
-    $("#hero .heros").responsiveSlides({
+    $(".heros").responsiveSlides({
         auto: true
     });
 
-    // $("footer .brands").owlCarousel({
-    //     autoPlay: 5000, //Set AutoPlay to 3 seconds
-    //     pagination: false,
-    //     itemScaleUp: true,
-    //     items : 7,
-    //     itemsMobile: [479,2],
-    //     itemsTablet: [768,3],
-    //     itemsDesktopSmall: [979, 5],
-    //     itemsDesktop: false
-    //
-    // });
+    $(".brands.schmuck").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        pagination: false,
+        itemScaleUp: true,
+        items : 4,
+        itemsMobile: [479,2],
+        itemsTablet: [768,3],
+        itemsDesktopSmall: [979, 5],
+        itemsDesktop: false
+
+    });
 
     google.maps.event.addDomListener(window, 'load', function initialize() {
         var latlng = new google.maps.LatLng(50.001058,12.083484);
@@ -71,7 +71,8 @@ $(function() {
             animation: google.maps.Animation.DROP
         });
 
-        var map = new google.maps.Map($('#kontakt .col-xs-12')[0], mapOptions);
+        var map = new google.maps.Map($('#anfahrt')[0], mapOptions);
         marker.setMap(map);
+        map.set('styles', [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.highway.controlled_access","elementType":"geometry","stylers":[{"color":"#99c018"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]);
     });
 });
